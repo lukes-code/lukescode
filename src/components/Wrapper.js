a70e5b521115a8e1a01f4d84b3e963fb96081176
lukes.code <lukesterry@gmail.com>
2020-02-19 07:58:08 +0000

    small styling changes

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Hacker from '../hacker.svg';
 
 class Wrapper extends React.Component {
     render () {
         return (
             <header>
-                <div className="wrapper-text">
-                    <p>hello im the wrapper</p>
+                <div className="wrapper-left">
+                    <p>Hello, my name</p>
+                    <p>is Luke!</p>
+                    <button className="btn">Contact</button>
+                </div>
+                <div className="wrapper-right">
+                    <img className="wrapper-img" src={Hacker} alt="coder"/>
                 </div>
             </header>
         );
