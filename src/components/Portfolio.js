625cd1a752128feec7da144b02ac951268b2c8f1
lukes.code <lukesterry@gmail.com>
2021-03-08 13:11:39 +0000

    askaway

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Infiniscroats from '../portfolio/infiniscroats.jpg';
 import Workify from '../portfolio/workify.jpg';
 import Corona from '../portfolio/corona.jpg';
+import AskAway from '../portfolio/askaway.jpg';
 import Omdb from '../portfolio/omdb.jpg';
 import Lukescode from '../portfolio/lukescode.jpg';
 import Todo from '../portfolio/todo.jpg';
@@ -11,6 +12,11 @@ class Portfolio extends React.Component {
         return (
             <div>
                 <div className="card">
+                <div className="portfolio-card">
+                        <a href="https://pleaseaskaway.netlify.app" target="_blank" rel="noopener noreferrer">
+                            <img src={Corona} alt="portfolio-img" className="portfolio-image" />
+                        </a>
+                    </div>
                     <div className="portfolio-card">
                         <a href="https://github.com/lukes-code/corona-dash" target="_blank" rel="noopener noreferrer">
                             <img src={Corona} alt="portfolio-img" className="portfolio-image" />
